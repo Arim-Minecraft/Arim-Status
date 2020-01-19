93b1a22175bebad98f52a307cdedaf1626e815d7
A248 <anandebeh@gmail.com>
2020-01-19 18:15:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,24 +2,33 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
- 
+var request = require('request');
+var mcCommand = '/status'; // Command for triggering
+var mcIP = 'arim.space'; // Your MC server IP or hostname address
+var mcPort = 25565; // Your MC server port (25565 is the default) 
 
-client.on('ready', () => {
-
-    console.log('I am ready!');
-
-});
-
- 
 
 client.on('message', message => {
-
-    if (message.content === 'ping') {
-
-       message.reply('pong');
-
-       }
-
+    if (message.content === mcCommand) {
+        var url = 'http://mcapi.us/server/status?ip=' + mcIP + '&port=' + mcPort;
+        request(url, function(err, response, body) {
+            if(err) {
+                console.log(err);
+                return message.reply('Error getting Minecraft server status.');
+            }
+            body = JSON.parse(body);
+            var status = 'Arim is currently offline :(';
+            if(body.online) {
+                status = 'Arim is **online** :D \n';
+                if(body.players.now) {
+                    status += '**' + body.players.now + '** current playing!';
+                } else {
+                    status += 'but no one is playing :(';
+                }
+            }
+            message.reply(status);
+        });
+    }
 });
 
  
