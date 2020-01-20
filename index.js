071b7c2c4a3e7b32559483d1efb620b32b0f301e
A248 <anandebeh@gmail.com>
2020-01-20 15:16:39 -0500

    add query and website checking

--- index.js
+++ index.js
@@ -2,16 +2,22 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
+const https = require('https');
+
 const request = require('request');
 const statusCmd = '/status';
 const mcIP = 'arim.space';
 const mcPort = 25565;
 
+const queryCmd = '/query';
+const Query = require("minecraft-query");
+const query = new Query(mcIP, mcPort, { timeout: 10000 });
 
-//const queryCmd = '/query';
-//const Query = require("minecraft-query");
-//const query = new Query(mcIP, mcPort, { timeout: 10000 });
+const isOnline = require('is-online');
 
+let playerCount = 0;
+
+boolean website = true;
 
 client.on('message', message => {
     if (message.content === statusCmd) {
@@ -35,7 +41,67 @@ client.on('message', message => {
         });
     }
 });
-
- 
+//guild id
+//568772431347449867
+//in-game channel id
+//629826592717144095
+//website channel id
+//668134910221090858
 
 client.login(process.env.BOT_TOKEN);
+
+function sendMessage(msg) {
+  client.guilds.get("568772431347449867").channels.get("629826592717144095").send(msg);
+}
+
+function sendWebsiteMessage(msg) {
+  client.guilds.get("568772431347449867").channels.get("668134910221090858").send(msg);
+}
+
+function handleQuery(res) {
+    const current = res.online_players;
+    if (current != playerCount) {
+      playerCount = current;
+      if (playerCount == 0) {
+        sendMessage("No more players online :(");
+      } else {
+        sendMessage("There are now " + playerCount + " players online.");
+      }
+    }
+}
+
+function handleDown(ex) {
+  if (playerCount != -1) {
+    playerCount = -1;
+    sendMessage("Uh-oh! Is the server down?");
+  }
+}
+
+function checkWebsite() {
+  https.get('https://www.arim.space/', function(res) {
+  if (res.statusCode != 200) {
+    if (website) {
+      sendWebsiteMessage("The website just went down!");
+    }
+
+  } else if (!website) {
+    sendWebsiteMessage("The website is back up!");
+  }
+  res.on('data', function(d) {
+
+  });
+
+  }).on('error', function(e) {
+    if (website) {
+      sendWebsiteMessage("Failed checking website status.");
+    }
+  });
+
+}
+
+async function checkAll() {
+  q.basicStat().then(success => handleQuery(success)).catch(ex => handleDown(ex));
+  checkWebsite();
+}
+
+setInterval(function() { checkAll(); }, 3000);
